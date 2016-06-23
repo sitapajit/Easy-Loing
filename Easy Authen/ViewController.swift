ed1264bd3b34cd5673174b87273655685a37e5a9
sitapa <sitapajit@gmail.com>
2016-06-23 14:24:56 +0700

    Login Finish

--- Easy Authen/ViewController.swift
+++ Easy Authen/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userTextField: UITextField!
+    
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    @IBAction func loginButton(sender: AnyObject) {
+        
+        let user = userTextField.text
+        let password = passwordTextField.text
+        
+        print("user ==> \(user)")
+        print("password ==> \(password)")
+        
+        checkSpace(user!, strPass: password!)
+        
+        
+        
+    }   //loginButton
+    
+    func checkSpace(strUser:String,strPass:String) -> Void {
+        if ((strUser.isEmpty) || (strPass.isEmpty)) {
+            print("Have Space")
+            
+            //Alert Dialog
+            let myAlert = UIAlertController(title: "มีช่องว่าง",message: "กรุณากรอกทุกช่องค่ะ",preferredStyle: UIAlertControllerStyle.Alert)
+            myAlert.addAction(UIAlertAction(title: "OK",style: UIAlertActionStyle.Default,handler: nil))
+            
+            self.presentViewController(myAlert, animated: true, completion: nil)
+            
+            
+            
+            
+        }
+    }   // funcHaveSpace
 
-}
+}   //  Class
 
